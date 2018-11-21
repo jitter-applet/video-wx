40704a17ca7bb19b5f14b75aa963a75e7ea8360b
SunAo <572220216@qq.com>
2018-11-22 06:59:28 +0800

    视频模块

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
-  serverUrl: "http://192.168.43.248:8081",
+  serverUrl: "http://localhost:8081",
+  //serverUrl: "http://192.168.43.248:8081",
   // serverUrl: "10.117.139.16",
   userInfo: null
 })
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -123,8 +123,32 @@ Page({
           }
         })
       },
+     
 
 
     })
+  }, uploadVideo: function () {
+    var me = this;
+    wx.chooseVideo({
+      sourceType: ['album'],
+      success: function (res) {
+        console.log(res);
+        var duration = res.duration;
+        var tmpHeight = res.height;
+        var tmpWidth = res.width;
+        var tmpVideoUrl = res.tempFilePath;
+        var tmpCoverUrl = res.thumbTempFilePath;
+        if(duration>10){
+          wx.showToast({
+            title:'视频长度不能超过10秒...',
+            icon:'none',
+            duration:2500
+          } )
+        }else{
+          //TODO 打开选择bgm页面
+        }
+      }
+    })
   }
+
 })
